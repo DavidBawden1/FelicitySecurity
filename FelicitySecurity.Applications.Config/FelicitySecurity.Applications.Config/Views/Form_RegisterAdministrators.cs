09ca1dfa3a706c7984300fe5f5c55e33a023db93
Dbawd <Dbawd@DESKTOP-T69TDHC>
2017-03-18 20:26:46 +0000

    individual button validation methods as different logic is necessary. Try catches removed. they arent necessary

--- FelicitySecurity.Applications.Config/FelicitySecurity.Applications.Config/Views/Form_RegisterAdministrators.cs
+++ FelicitySecurity.Applications.Config/FelicitySecurity.Applications.Config/Views/Form_RegisterAdministrators.cs
@@ -22,6 +22,7 @@ namespace FelicitySecurity.Applications.Config
         #endregion
 
         #region Properties 
+        private string _error;
         /// <summary>
         /// Validate the controls here. if validation fails return the specific error message.  
         /// </summary>
@@ -29,42 +30,83 @@ namespace FelicitySecurity.Applications.Config
         {
             get
             {
-                if (string.IsNullOrEmpty(CreateUsername_TextBox.Text) && string.IsNullOrEmpty(EnterEmail_TextBox.Text)
-                    && string.IsNullOrEmpty(EnterPin_TextBox.Text) && string.IsNullOrEmpty(ReEnterPin_TextBox.Text))
-                {
-                    return "Please fill in your details!";
-                }
-                if (string.IsNullOrEmpty(CreateUsername_TextBox.Text))
-                {
-                    return "You must supply a username!";
-                }
-                if (CreateUsername_TextBox.TextLength > 50)
-                {
-                    return "Your username must be between 0 and 50 characters!";
-                }
-                if (string.IsNullOrEmpty(EnterEmail_TextBox.Text))
-                {
-                    return "You must supply an email address!";
-                }
-                if (!validation.IsValidEmail(EnterEmail_TextBox.Text))
-                {
-                    return "Your email address has to be valid! eg. woopiegoldberg@yahoo.co.uk";
-                }
-                if (string.IsNullOrEmpty(EnterPin_TextBox.Text) || EnterPin_TextBox.TextLength > 4)
-                {
-                    return "You must enter a 4 digit pin!";
-                }
-                if (EnterPin_TextBox.Text != ReEnterPin_TextBox.Text)
-                {
-                    return "please confirm your pincodes!";
-                }
-                if (ValidateEmail.DoesEmailExist(EnterEmail_TextBox.Text))
-                {
-                    return EnterEmail_TextBox.Text + " already exists!";
-                }
-
-                return string.Empty;
+                _error =ValidateRegisterButtonClick();
+                _error = ValidateUpdateButtonClick();
+                return _error;
             }
+        }
+
+        private string ValidateRegisterButtonClick()
+        {
+            if (string.IsNullOrEmpty(CreateUsername_TextBox.Text) && string.IsNullOrEmpty(EnterEmail_TextBox.Text)
+                                && string.IsNullOrEmpty(EnterPin_TextBox.Text) && string.IsNullOrEmpty(ReEnterPin_TextBox.Text))
+            {
+                return "Please fill in your details!";
+            }
+            if (string.IsNullOrEmpty(CreateUsername_TextBox.Text))
+            {
+                return "You must supply a username!";
+            }
+            if (CreateUsername_TextBox.TextLength > 50)
+            {
+                return "Your username must be between 0 and 50 characters!";
+            }
+            if (string.IsNullOrEmpty(EnterEmail_TextBox.Text))
+            {
+                return "You must supply an email address!";
+            }
+            if (!validation.IsValidEmail(EnterEmail_TextBox.Text))
+            {
+                return "Your email address has to be valid! eg. woopiegoldberg@yahoo.co.uk";
+            }
+            if (string.IsNullOrEmpty(EnterPin_TextBox.Text) || EnterPin_TextBox.TextLength > 4)
+            {
+                return "You must enter a 4 digit pin!";
+            }
+            if (EnterPin_TextBox.Text != ReEnterPin_TextBox.Text)
+            {
+                return "please confirm your pincodes!";
+            }
+            if (ValidateEmail.DoesEmailExist(EnterEmail_TextBox.Text))
+            {
+                return EnterEmail_TextBox.Text + " already exists!";
+            }
+
+            return string.Empty;
+        }
+
+        private string ValidateUpdateButtonClick()
+        {
+            if (string.IsNullOrEmpty(CreateUsername_TextBox.Text) && string.IsNullOrEmpty(EnterEmail_TextBox.Text)
+                                && string.IsNullOrEmpty(EnterPin_TextBox.Text) && string.IsNullOrEmpty(ReEnterPin_TextBox.Text))
+            {
+                return "Please fill in your details!";
+            }
+            if (string.IsNullOrEmpty(CreateUsername_TextBox.Text))
+            {
+                return "You must supply a username!";
+            }
+            if (CreateUsername_TextBox.TextLength > 50)
+            {
+                return "Your username must be between 0 and 50 characters!";
+            }
+            if (string.IsNullOrEmpty(EnterEmail_TextBox.Text))
+            {
+                return "You must supply an email address!";
+            }
+            if (!validation.IsValidEmail(EnterEmail_TextBox.Text))
+            {
+                return "Your email address has to be valid! eg. woopiegoldberg@yahoo.co.uk";
+            }
+            if (string.IsNullOrEmpty(EnterPin_TextBox.Text) || EnterPin_TextBox.TextLength > 4)
+            {
+                return "You must enter a 4 digit pin!";
+            }
+            if (EnterPin_TextBox.Text != ReEnterPin_TextBox.Text)
+            {
+                return "please confirm your pincodes!";
+            }
+            return string.Empty;
         }
 
         /// <summary>
@@ -105,49 +147,32 @@ namespace FelicitySecurity.Applications.Config
         /// <param name="e"></param>
         private void Register_Button_Click(object sender, EventArgs e)
         {
-            try
+            //if error is null then validation has passed so continue otherwise return the error message. 
+            if (string.IsNullOrEmpty(Error.ToString()))
             {
-                //if error is null then validation has passed so continue otherwise return the error message. 
-                if (string.IsNullOrEmpty(Error.ToString()))
-                {
-                    viewModel.BindTextboxControls(this, viewModel, _textbox);
-                    controller.AddAdministrators(EnterEmail_TextBox.Text, CreateUsername_TextBox.Text, EnterPin_TextBox.Text);
-                    MessageBox.Show("Administrator added successfully.", "Felicity Security", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    viewModel.DisplayAdministratorEmails(this, controller, model, sortingType);
-                }
-                else
-                {
-                    MessageBox.Show(Error, "Felicity Security", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
+                viewModel.BindTextboxControls(this, viewModel, _textbox);
+                controller.AddAdministrators(EnterEmail_TextBox.Text, CreateUsername_TextBox.Text, EnterPin_TextBox.Text);
+                MessageBox.Show("Administrator added successfully.", "Felicity Security", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                viewModel.DisplayAdministratorEmails(this, controller, model, sortingType);
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show(ex.Message, "Felicity Security", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
+                MessageBox.Show(Error, "Felicity Security", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void UpdateAdministratorButton_Click(object sender, EventArgs e)
         {
-            try
+            if (string.IsNullOrEmpty(Error.ToString()))
             {
-                //if error is null then validation has passed so continue otherwise return the error message. 
-                if (string.IsNullOrEmpty(Error.ToString()))
-                {
-                    viewModel.BindTextboxControls(this, viewModel, _textbox);
-                    controller.UpdateSelectedAdministrator(EnterEmail_TextBox.Text, CreateUsername_TextBox.Text, EnterPin_TextBox.Text);
-                    MessageBox.Show("Administrator updated successfully.", "Felicity Security", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    viewModel.DisplayAdministratorEmails(this, controller, model, sortingType);
-                }
-                else
-                {
-                    MessageBox.Show(Error, "Felicity Security", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
+                viewModel.BindTextboxControls(this, viewModel, _textbox);
+                controller.UpdateSelectedAdministrator(EnterEmail_TextBox.Text, CreateUsername_TextBox.Text, EnterPin_TextBox.Text);
+                MessageBox.Show("Administrator updated successfully.", "Felicity Security", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                viewModel.DisplayAdministratorEmails(this, controller, model, sortingType);
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show(ex.Message, "Felicity Security", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
+                MessageBox.Show(Error, "Felicity Security", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
