fe9e879573f133920d45b33e7c79d4450961f395
Dbawd <Dbawd@DESKTOP-T69TDHC>
2017-03-21 21:34:08 +0000

    added controls

--- FelicitySecurity.Applications.Config/FelicitySecurity.Applications.Config/Views/Form_AuthenticateAdministrator.Designer.cs
+++ FelicitySecurity.Applications.Config/FelicitySecurity.Applications.Config/Views/Form_AuthenticateAdministrator.Designer.cs
@@ -36,6 +36,15 @@
             this.AuthenticationFormCredentialsBackgroud_GroupBox = new System.Windows.Forms.GroupBox();
             this.AuthenticationHeadingAndCredentials_TableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.AuthenticationCredentials_GroupBox = new System.Windows.Forms.GroupBox();
+            this.AuthenticationTextBoxControls_TableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.AuthenticationTextBoxControls_GroupBox = new System.Windows.Forms.GroupBox();
+            this.PinCode_TextBox = new System.Windows.Forms.TextBox();
+            this.EmailAddress_TextBox = new System.Windows.Forms.TextBox();
+            this.EnterEmailAddress_Label = new System.Windows.Forms.Label();
+            this.EnterPinCode_Label = new System.Windows.Forms.Label();
+            this.AuthenticationButtonControls_TableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.SignIn_Button = new System.Windows.Forms.Button();
+            this.Clear_Button = new System.Windows.Forms.Button();
             this.AuthenticationHeader_GroupBox = new System.Windows.Forms.GroupBox();
             this.AuthenticationDetailsBackground_GroupBox = new System.Windows.Forms.GroupBox();
             this.AuthenticationAdministratorsDetails_TableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
@@ -44,22 +53,18 @@
             this.AdministratorSorting_GroupBox = new System.Windows.Forms.GroupBox();
             this.AdministratorsGroup_Box = new System.Windows.Forms.GroupBox();
             this.AuthenticationLogo_GroupBox = new System.Windows.Forms.GroupBox();
-            this.AuthenticationTextBoxControls_TableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.AuthenticationTextBoxControls_GroupBox = new System.Windows.Forms.GroupBox();
-            this.AuthenticationButtonControls_TableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.SignIn_Button = new System.Windows.Forms.Button();
-            this.Clear_Button = new System.Windows.Forms.Button();
             this.GeneralControls_MenuStrip.SuspendLayout();
             this.AuthenticationBackground_TableLayoutPanel.SuspendLayout();
             this.AuthenticationFormCredentialsBackgroud_GroupBox.SuspendLayout();
             this.AuthenticationHeadingAndCredentials_TableLayoutPanel.SuspendLayout();
             this.AuthenticationCredentials_GroupBox.SuspendLayout();
+            this.AuthenticationTextBoxControls_TableLayoutPanel.SuspendLayout();
+            this.AuthenticationTextBoxControls_GroupBox.SuspendLayout();
+            this.AuthenticationButtonControls_TableLayoutPanel.SuspendLayout();
             this.AuthenticationDetailsBackground_GroupBox.SuspendLayout();
             this.AuthenticationAdministratorsDetails_TableLayoutPanel.SuspendLayout();
             this.AdministratorsBackground_GroupBox.SuspendLayout();
             this.AdministratorsList_TableLayoutPanel.SuspendLayout();
-            this.AuthenticationTextBoxControls_TableLayoutPanel.SuspendLayout();
-            this.AuthenticationButtonControls_TableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // GeneralControls_MenuStrip
@@ -83,14 +88,14 @@
             // Back_MenuItem
             // 
             this.Back_MenuItem.Name = "Back_MenuItem";
-            this.Back_MenuItem.Size = new System.Drawing.Size(152, 22);
+            this.Back_MenuItem.Size = new System.Drawing.Size(137, 22);
             this.Back_MenuItem.Text = "Registration";
             this.Back_MenuItem.Click += new System.EventHandler(this.Back_MenuItem_Click);
             // 
             // Close_MenuItem
             // 
             this.Close_MenuItem.Name = "Close_MenuItem";
-            this.Close_MenuItem.Size = new System.Drawing.Size(152, 22);
+            this.Close_MenuItem.Size = new System.Drawing.Size(137, 22);
             this.Close_MenuItem.Text = "Close";
             // 
             // AuthenticationBackground_TableLayoutPanel
@@ -143,6 +148,103 @@
             this.AuthenticationCredentials_GroupBox.TabIndex = 0;
             this.AuthenticationCredentials_GroupBox.TabStop = false;
             this.AuthenticationCredentials_GroupBox.Text = "Authentication Credentials";
+            // 
+            // AuthenticationTextBoxControls_TableLayoutPanel
+            // 
+            this.AuthenticationTextBoxControls_TableLayoutPanel.ColumnCount = 1;
+            this.AuthenticationTextBoxControls_TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.AuthenticationTextBoxControls_TableLayoutPanel.Controls.Add(this.AuthenticationTextBoxControls_GroupBox, 0, 0);
+            this.AuthenticationTextBoxControls_TableLayoutPanel.Controls.Add(this.AuthenticationButtonControls_TableLayoutPanel, 0, 1);
+            this.AuthenticationTextBoxControls_TableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AuthenticationTextBoxControls_TableLayoutPanel.Location = new System.Drawing.Point(3, 16);
+            this.AuthenticationTextBoxControls_TableLayoutPanel.Name = "AuthenticationTextBoxControls_TableLayoutPanel";
+            this.AuthenticationTextBoxControls_TableLayoutPanel.RowCount = 2;
+            this.AuthenticationTextBoxControls_TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.AuthenticationTextBoxControls_TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.AuthenticationTextBoxControls_TableLayoutPanel.Size = new System.Drawing.Size(282, 199);
+            this.AuthenticationTextBoxControls_TableLayoutPanel.TabIndex = 0;
+            // 
+            // AuthenticationTextBoxControls_GroupBox
+            // 
+            this.AuthenticationTextBoxControls_GroupBox.Controls.Add(this.EnterEmailAddress_Label);
+            this.AuthenticationTextBoxControls_GroupBox.Controls.Add(this.PinCode_TextBox);
+            this.AuthenticationTextBoxControls_GroupBox.Controls.Add(this.EmailAddress_TextBox);
+            this.AuthenticationTextBoxControls_GroupBox.Controls.Add(this.EnterPinCode_Label);
+            this.AuthenticationTextBoxControls_GroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AuthenticationTextBoxControls_GroupBox.Location = new System.Drawing.Point(3, 3);
+            this.AuthenticationTextBoxControls_GroupBox.Name = "AuthenticationTextBoxControls_GroupBox";
+            this.AuthenticationTextBoxControls_GroupBox.Size = new System.Drawing.Size(276, 153);
+            this.AuthenticationTextBoxControls_GroupBox.TabIndex = 0;
+            this.AuthenticationTextBoxControls_GroupBox.TabStop = false;
+            // 
+            // PinCode_TextBox
+            // 
+            this.PinCode_TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PinCode_TextBox.Location = new System.Drawing.Point(6, 40);
+            this.PinCode_TextBox.Name = "PinCode_TextBox";
+            this.PinCode_TextBox.Size = new System.Drawing.Size(264, 20);
+            this.PinCode_TextBox.TabIndex = 1;
+            // 
+            // EmailAddress_TextBox
+            // 
+            this.EmailAddress_TextBox.Location = new System.Drawing.Point(6, 96);
+            this.EmailAddress_TextBox.Name = "EmailAddress_TextBox";
+            this.EmailAddress_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.EmailAddress_TextBox.TabIndex = 0;
+            // 
+            // EnterEmailAddress_Label
+            // 
+            this.EnterEmailAddress_Label.AutoSize = true;
+            this.EnterEmailAddress_Label.Location = new System.Drawing.Point(3, 24);
+            this.EnterEmailAddress_Label.Name = "EnterEmailAddress_Label";
+            this.EnterEmailAddress_Label.Size = new System.Drawing.Size(101, 13);
+            this.EnterEmailAddress_Label.TabIndex = 2;
+            this.EnterEmailAddress_Label.Text = "Enter Email Address";
+            // 
+            // EnterPinCode_Label
+            // 
+            this.EnterPinCode_Label.AutoSize = true;
+            this.EnterPinCode_Label.Location = new System.Drawing.Point(3, 80);
+            this.EnterPinCode_Label.Name = "EnterPinCode_Label";
+            this.EnterPinCode_Label.Size = new System.Drawing.Size(78, 13);
+            this.EnterPinCode_Label.TabIndex = 3;
+            this.EnterPinCode_Label.Text = "Enter Pin Code";
+            // 
+            // AuthenticationButtonControls_TableLayoutPanel
+            // 
+            this.AuthenticationButtonControls_TableLayoutPanel.ColumnCount = 4;
+            this.AuthenticationButtonControls_TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.AuthenticationButtonControls_TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.AuthenticationButtonControls_TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.AuthenticationButtonControls_TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.AuthenticationButtonControls_TableLayoutPanel.Controls.Add(this.SignIn_Button, 0, 0);
+            this.AuthenticationButtonControls_TableLayoutPanel.Controls.Add(this.Clear_Button, 1, 0);
+            this.AuthenticationButtonControls_TableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AuthenticationButtonControls_TableLayoutPanel.Location = new System.Drawing.Point(3, 162);
+            this.AuthenticationButtonControls_TableLayoutPanel.Name = "AuthenticationButtonControls_TableLayoutPanel";
+            this.AuthenticationButtonControls_TableLayoutPanel.RowCount = 1;
+            this.AuthenticationButtonControls_TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.AuthenticationButtonControls_TableLayoutPanel.Size = new System.Drawing.Size(276, 34);
+            this.AuthenticationButtonControls_TableLayoutPanel.TabIndex = 1;
+            // 
+            // SignIn_Button
+            // 
+            this.SignIn_Button.Location = new System.Drawing.Point(3, 3);
+            this.SignIn_Button.Name = "SignIn_Button";
+            this.SignIn_Button.Size = new System.Drawing.Size(63, 23);
+            this.SignIn_Button.TabIndex = 0;
+            this.SignIn_Button.Text = "Sign in ";
+            this.SignIn_Button.UseVisualStyleBackColor = true;
+            // 
+            // Clear_Button
+            // 
+            this.Clear_Button.Location = new System.Drawing.Point(72, 3);
+            this.Clear_Button.Name = "Clear_Button";
+            this.Clear_Button.Size = new System.Drawing.Size(63, 23);
+            this.Clear_Button.TabIndex = 1;
+            this.Clear_Button.Text = "Clear";
+            this.Clear_Button.UseVisualStyleBackColor = true;
             // 
             // AuthenticationHeader_GroupBox
             // 
@@ -232,65 +334,6 @@
             this.AuthenticationLogo_GroupBox.TabIndex = 1;
             this.AuthenticationLogo_GroupBox.TabStop = false;
             // 
-            // AuthenticationTextBoxControls_TableLayoutPanel
-            // 
-            this.AuthenticationTextBoxControls_TableLayoutPanel.ColumnCount = 1;
-            this.AuthenticationTextBoxControls_TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.AuthenticationTextBoxControls_TableLayoutPanel.Controls.Add(this.AuthenticationTextBoxControls_GroupBox, 0, 0);
-            this.AuthenticationTextBoxControls_TableLayoutPanel.Controls.Add(this.AuthenticationButtonControls_TableLayoutPanel, 0, 1);
-            this.AuthenticationTextBoxControls_TableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.AuthenticationTextBoxControls_TableLayoutPanel.Location = new System.Drawing.Point(3, 16);
-            this.AuthenticationTextBoxControls_TableLayoutPanel.Name = "AuthenticationTextBoxControls_TableLayoutPanel";
-            this.AuthenticationTextBoxControls_TableLayoutPanel.RowCount = 2;
-            this.AuthenticationTextBoxControls_TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
-            this.AuthenticationTextBoxControls_TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.AuthenticationTextBoxControls_TableLayoutPanel.Size = new System.Drawing.Size(282, 199);
-            this.AuthenticationTextBoxControls_TableLayoutPanel.TabIndex = 0;
-            // 
-            // AuthenticationTextBoxControls_GroupBox
-            // 
-            this.AuthenticationTextBoxControls_GroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.AuthenticationTextBoxControls_GroupBox.Location = new System.Drawing.Point(3, 3);
-            this.AuthenticationTextBoxControls_GroupBox.Name = "AuthenticationTextBoxControls_GroupBox";
-            this.AuthenticationTextBoxControls_GroupBox.Size = new System.Drawing.Size(276, 153);
-            this.AuthenticationTextBoxControls_GroupBox.TabIndex = 0;
-            this.AuthenticationTextBoxControls_GroupBox.TabStop = false;
-            // 
-            // AuthenticationButtonControls_TableLayoutPanel
-            // 
-            this.AuthenticationButtonControls_TableLayoutPanel.ColumnCount = 4;
-            this.AuthenticationButtonControls_TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.AuthenticationButtonControls_TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.AuthenticationButtonControls_TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.AuthenticationButtonControls_TableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.AuthenticationButtonControls_TableLayoutPanel.Controls.Add(this.SignIn_Button, 0, 0);
-            this.AuthenticationButtonControls_TableLayoutPanel.Controls.Add(this.Clear_Button, 1, 0);
-            this.AuthenticationButtonControls_TableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.AuthenticationButtonControls_TableLayoutPanel.Location = new System.Drawing.Point(3, 162);
-            this.AuthenticationButtonControls_TableLayoutPanel.Name = "AuthenticationButtonControls_TableLayoutPanel";
-            this.AuthenticationButtonControls_TableLayoutPanel.RowCount = 1;
-            this.AuthenticationButtonControls_TableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.AuthenticationButtonControls_TableLayoutPanel.Size = new System.Drawing.Size(276, 34);
-            this.AuthenticationButtonControls_TableLayoutPanel.TabIndex = 1;
-            // 
-            // SignIn_Button
-            // 
-            this.SignIn_Button.Location = new System.Drawing.Point(3, 3);
-            this.SignIn_Button.Name = "SignIn_Button";
-            this.SignIn_Button.Size = new System.Drawing.Size(63, 23);
-            this.SignIn_Button.TabIndex = 0;
-            this.SignIn_Button.Text = "Sign in ";
-            this.SignIn_Button.UseVisualStyleBackColor = true;
-            // 
-            // Clear_Button
-            // 
-            this.Clear_Button.Location = new System.Drawing.Point(72, 3);
-            this.Clear_Button.Name = "Clear_Button";
-            this.Clear_Button.Size = new System.Drawing.Size(63, 23);
-            this.Clear_Button.TabIndex = 1;
-            this.Clear_Button.Text = "Clear";
-            this.Clear_Button.UseVisualStyleBackColor = true;
-            // 
             // AuthenticateAdministrators_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -308,12 +351,14 @@
             this.AuthenticationFormCredentialsBackgroud_GroupBox.ResumeLayout(false);
             this.AuthenticationHeadingAndCredentials_TableLayoutPanel.ResumeLayout(false);
             this.AuthenticationCredentials_GroupBox.ResumeLayout(false);
+            this.AuthenticationTextBoxControls_TableLayoutPanel.ResumeLayout(false);
+            this.AuthenticationTextBoxControls_GroupBox.ResumeLayout(false);
+            this.AuthenticationTextBoxControls_GroupBox.PerformLayout();
+            this.AuthenticationButtonControls_TableLayoutPanel.ResumeLayout(false);
             this.AuthenticationDetailsBackground_GroupBox.ResumeLayout(false);
             this.AuthenticationAdministratorsDetails_TableLayoutPanel.ResumeLayout(false);
             this.AdministratorsBackground_GroupBox.ResumeLayout(false);
             this.AdministratorsList_TableLayoutPanel.ResumeLayout(false);
-            this.AuthenticationTextBoxControls_TableLayoutPanel.ResumeLayout(false);
-            this.AuthenticationButtonControls_TableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -342,5 +387,9 @@
         private System.Windows.Forms.TableLayoutPanel AuthenticationButtonControls_TableLayoutPanel;
         private System.Windows.Forms.Button SignIn_Button;
         private System.Windows.Forms.Button Clear_Button;
+        private System.Windows.Forms.TextBox PinCode_TextBox;
+        private System.Windows.Forms.TextBox EmailAddress_TextBox;
+        private System.Windows.Forms.Label EnterEmailAddress_Label;
+        private System.Windows.Forms.Label EnterPinCode_Label;
     }
 }
