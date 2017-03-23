98907c2afcb12ae93040778f3bf03ef71632d52e
Dbawd <Dbawd@DESKTOP-T69TDHC>
2017-03-23 20:18:47 +0000

    Added register members form.

---
+++ FelicitySecurity.Applications.Config/FelicitySecurity.Applications.Config/Views/RegisterMembers_Form.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace FelicitySecurity.Applications.Config.Views
+{
+    partial class RegisterMembers_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // RegisterMembers_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 569);
+            this.MinimumSize = new System.Drawing.Size(1025, 608);
+            this.Name = "RegisterMembers_Form";
+            this.Text = "Register Members";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
