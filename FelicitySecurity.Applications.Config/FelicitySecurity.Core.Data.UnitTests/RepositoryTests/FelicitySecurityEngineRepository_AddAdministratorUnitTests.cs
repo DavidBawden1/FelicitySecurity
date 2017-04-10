68b5486519f3106218364f7f4f4aa8b366308158
Dbawd <Dbawd@DESKTOP-T69TDHC>
2017-04-10 22:47:25 +0100

    added repository tests for administrators. need to mock repository context.

---
+++ FelicitySecurity.Applications.Config/FelicitySecurity.Core.Data.UnitTests/RepositoryTests/FelicitySecurityEngineRepository_AddAdministratorUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FelicitySecurity.Core.Data.Repository;
+using FelicitySecurity.Core.DataTransferObjects;
+using System.Collections.Generic;
+using FelicitySecurity.Core.Data.UnitTests.Helpers;
+
+namespace FelicitySecurity.Core.Data.UnitTests
+{
+    [TestClass]
+    public class FelicitySecurityEngineRepository_AddAdministratorUnitTests
+    {
+        [TestMethod]
+        public void TestAddAdministrators()
+        {
+            FelicitySecurityRepository repository = new FelicitySecurityRepository();
+            List<Administrators_dto> listOfAdministrators = new List<Administrators_dto>();
+            listOfAdministrators.Add(DataSeedHelper.CreateAdministrator(repository, "dbawden@outlook.com", "David", "Bawden"));
+            Assert.AreEqual(1, listOfAdministrators.Count);
+            Assert.AreNotEqual(0, listOfAdministrators.Count);
+        }
+    }
+}
