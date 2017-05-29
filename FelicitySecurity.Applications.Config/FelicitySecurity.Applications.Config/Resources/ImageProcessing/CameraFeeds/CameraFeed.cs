65cf802ffe0a52cc00cdac664ccb97073ee4c7f1
Dbawd <Dbawd@DESKTOP-T69TDHC>
2017-05-29 20:28:28 +0100

    modularized methods.

--- FelicitySecurity.Applications.Config/FelicitySecurity.Applications.Config/Resources/ImageProcessing/CameraFeeds/CameraFeed.cs
+++ FelicitySecurity.Applications.Config/FelicitySecurity.Applications.Config/Resources/ImageProcessing/CameraFeeds/CameraFeed.cs
@@ -1,6 +1,7 @@
 ﻿using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using Emgu.CV.UI;
 using FelicitySecurity.Applications.Config.Resources.ImageProcessing.FaceRecognition;
 using FelicitySecurity.Applications.Config.Views;
 using System;
@@ -14,7 +15,7 @@ namespace FelicitySecurity.Applications.Config.Resources.ImageProcessing.CameraF
         #region Declarations
         RegisterMembers_Form registerMembersForm = new RegisterMembers_Form();
         SuspectFacialPrediction suspectFacialPrediction = new SuspectFacialPrediction();
-        
+
         #endregion
         #region Properties
         private int _captureInstance = 0;
@@ -29,12 +30,13 @@ namespace FelicitySecurity.Applications.Config.Resources.ImageProcessing.CameraF
 
             }
         }
+        private Capture _channelFeed;
         public Capture ChannelFeed
         {
             get
             {
-                ChannelFeed =  new Capture(CaptureInstance);
-                return ChannelFeed;
+                _channelFeed = new Capture(CaptureInstance);
+                return _channelFeed;
             }
             set
             {
@@ -58,7 +60,7 @@ namespace FelicitySecurity.Applications.Config.Resources.ImageProcessing.CameraF
             get;
             set;
         }
-        
+
         public Image<Gray, Byte> GrayscaledCroppedFace
         {
             get;
@@ -78,15 +80,26 @@ namespace FelicitySecurity.Applications.Config.Resources.ImageProcessing.CameraF
         #endregion
 
         #region Methods
-        public Emgu.CV.IImage GetFoundFace(Image<Gray, Byte> ImgFound, string NameOfMember, string lastname, string postcode, int MatchValue)
+        public Emgu.CV.IImage GetFoundFace(Image<Gray, Byte> imageOfFoundFace)
         {
-            return registerMembersForm.RecognisedMember_EmguImageBox.Image = ImgFound;
+            return registerMembersForm.RecognisedMember_EmguImageBox.Image = imageOfFoundFace;
         }
-        public void DisplayFoundFace(Emgu.CV.UI.ImageBox imageBox, Image<Gray, Byte> ImgFound, string NameOfMember, string lastname, string postcode, int MatchValue)
+
+        /// <summary>
+        /// Returns the the found face to the image box
+        /// </summary>
+        /// <param name="imageBoxToDisplayFoundFace"></param>
+        /// <param name="imageOfFoundFace"></param>
+        /// <param name="suspectFirstName"></param>
+        /// <param name="suspectLastName"></param>
+        /// <param name="suspectPostCode"></param>
+        /// <param name="matchValue"></param>
+        public ImageBox DisplayFoundFace(ImageBox imageBoxToDisplayFoundFace, Image<Gray, Byte> imageOfFoundFace)
         {
-            imageBox.Image = ImgFound;
+            imageBoxToDisplayFoundFace.Image = imageOfFoundFace;
+            return imageBoxToDisplayFoundFace;
         }
-    
+
         #endregion
 
         /// <summary>
@@ -100,55 +113,99 @@ namespace FelicitySecurity.Applications.Config.Resources.ImageProcessing.CameraF
                 {
                     if (RawCameraFeedImage != null)
                     {
-                        registerMembersForm.CameraFeed_ImageBox.BackColor = Color.Transparent;
-                        registerMembersForm.CameraFeed_ImageBox.BackgroundImage = null;
-                        var grayscaleFrameImage = RawCameraFeedImage.Convert<Gray, Byte>();
-                        Rectangle[] Faces =
-                        Cascade.DetectMultiScale(grayscaleFrameImage, 8.0, 1, new Size(100, 100), new Size(800, 800)); //run cascade file over gray image with precision of three as 1.2 is very fast but inaccurate, 3.0 is slower but accurate and 0 min next neighbours rather than 10 because of accuracy too. 
-
-                        //if i is less than the length of data within the faces array remove it from image 
-                        for (int i = 0; i < Faces.Length; i++)
-                        {
-                            Faces[i].X += (int)(Faces[i].Height * 0.20);// enlarge image of face
-                            Faces[i].Y += (int)(Faces[i].Width * 0.30);//enlarge image of face 
-                            Faces[i].Height -= (int)(Faces[i].Height * 0.3);//remove anything that isnt a face
-                            Faces[i].Width -= (int)(Faces[i].Width * 0.35);//remove anything that isnt a face
-
-                            GrayscaledCroppedFace = RawCameraFeedImage.Copy(Faces[i]).Convert<Gray, Byte>().Resize(100, 100, Inter.Cubic);
-                            GrayscaledCroppedFace._EqualizeHist();
-                            //draw face detected in usersface imagebox 
-                            RawCameraFeedImage.Draw(Faces[i], new Bgr(Color.Orange), 1);
-                            //if faces are recognised then display to the user
-
-                            if (suspectFacialPrediction.IsDataSetPopulated)
-                            {
-                                Thread.Sleep(20);
-                                string name = suspectFacialPrediction.GetPositiveMatchOnFacialRecognition(GrayscaledCroppedFace);
-                                string lastname = suspectFacialPrediction.GetPositiveMatchOnFacialRecognition(GrayscaledCroppedFace);
-                                string postcode = suspectFacialPrediction.GetPositiveMatchOnFacialRecognition(GrayscaledCroppedFace);
-                                int MatchValue = (int)suspectFacialPrediction.NeighbourDistance;
-
-                                GetFoundFace(GrayscaledCroppedFace, name, lastname, postcode, MatchValue);
-                                Suspect suspect = new Suspect();
-                                suspect.FirstName = name;
-                                registerMembersForm.GetSuspectDetails(suspect);
-                            }
-                        }
+                        registerMembersForm.CameraFeed_ImageBox = SetWorkingCameraFeedProperties();
+                        Rectangle[] detectedFace = GetDetectedFace();
+                        GetCroppedDetectedFace(detectedFace);
+                        RecogniseDetectedFace();
                     }
                     else
                     {
-                        //UsersFace.back
-                        registerMembersForm.CameraFeed_ImageBox.BackColor = Color.Black;
+                        DisplaySwitchedOffCameraFeed();
                     }
-                    registerMembersForm.CameraFeed_ImageBox.Image = RawCameraFeedImage;//show feed 
-                    registerMembersForm.CroppedDetectedFace_EmguImageBox.Image = GrayscaledCroppedFace;//show new resized feed
+                    ReturnCameraFeedsToUI();
                 }
-
             }
             catch (Exception ex)
             {
                 return;
             }
+        }
+
+        /// <summary>
+        /// Displays the camera feed to the UI controls on the form. 
+        /// </summary>
+        private void ReturnCameraFeedsToUI()
+        {
+            registerMembersForm.CameraFeed_ImageBox.Image = RawCameraFeedImage;//show feed 
+            registerMembersForm.CroppedDetectedFace_EmguImageBox.Image = GrayscaledCroppedFace;//show new resized feed
+        }
+
+        /// <summary>
+        /// Displays a broken Circuit image to the UI control to resemble no camera feed activity.
+        /// </summary>
+        /// <returns>Imagebox</returns>
+        private ImageBox DisplaySwitchedOffCameraFeed()
+        {
+            registerMembersForm.CameraFeed_ImageBox.BackColor = Color.Black;
+            return registerMembersForm.CameraFeed_ImageBox;
+        }
+
+        /// <summary>
+        /// When a face is detected, the face is cropped & enlarged. Then a square is drawn over it.
+        /// </summary>
+        /// <param name="detectedFaces"></param>
+        private void GetCroppedDetectedFace(Rectangle[] detectedFaces)
+        {
+            for (int i = 0; i < detectedFaces.Length; i++)
+            {
+                detectedFaces[i].X += (int)(detectedFaces[i].Height * 0.20);// enlarge image of face
+                detectedFaces[i].Y += (int)(detectedFaces[i].Width * 0.30);//enlarge image of face 
+                detectedFaces[i].Height -= (int)(detectedFaces[i].Height * 0.3);//remove anything that isnt a face
+                detectedFaces[i].Width -= (int)(detectedFaces[i].Width * 0.35);//remove anything that isnt a face
+                GrayscaledCroppedFace = RawCameraFeedImage.Copy(detectedFaces[i]).Convert<Gray, Byte>().Resize(100, 100, Inter.Cubic);
+                GrayscaledCroppedFace._EqualizeHist();
+                RawCameraFeedImage.Draw(detectedFaces[i], new Bgr(Color.Orange), 1);
+            }
+        }
+
+        /// <summary>
+        /// Recognises the details of the detected subject
+        /// </summary>
+        private void RecogniseDetectedFace()
+        {
+            if (suspectFacialPrediction.IsDataSetPopulated)
+            {
+                Suspect suspect = new Suspect();
+                suspect.FirstName = suspectFacialPrediction.GetPositiveMatchOnFacialRecognition(GrayscaledCroppedFace);
+                suspect.LastName = suspectFacialPrediction.GetPositiveMatchOnFacialRecognition(GrayscaledCroppedFace);
+                suspect.PostCode = suspectFacialPrediction.GetPositiveMatchOnFacialRecognition(GrayscaledCroppedFace);
+                int matchValue = (int)suspectFacialPrediction.NeighbourDistance;
+                GetFoundFace(GrayscaledCroppedFace);
+                registerMembersForm.GetSuspectDetails(suspect);
+            }
+        }
+
+        /// <summary>
+        /// Returns a detected face
+        /// </summary>
+        /// <returns>a rectangle array containing a face within the camerafeed</returns>
+        private Rectangle[] GetDetectedFace()
+        {
+            var grayscaleFrameImage = RawCameraFeedImage.Convert<Gray, Byte>();
+            Rectangle[] detectedFace =
+            Cascade.DetectMultiScale(grayscaleFrameImage, 8.0, 1, new Size(100, 100), new Size(800, 800));
+            return detectedFace;
+        }
+
+        /// <summary>
+        /// Configures the camera feeds Image box properties to that of an operational camera. 
+        /// </summary>
+        /// <returns>an image box</returns>
+        private ImageBox SetWorkingCameraFeedProperties()
+        {
+            registerMembersForm.CameraFeed_ImageBox.BackColor = Color.Transparent;
+            registerMembersForm.CameraFeed_ImageBox.BackgroundImage = null;
+            return registerMembersForm.CameraFeed_ImageBox;
         }
     }
 }
