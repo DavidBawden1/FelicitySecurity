6f8ac7ac9c8c1d9a51ca9df4bb0fe6596791cc7f
Dbawd <Dbawd@DESKTOP-T69TDHC>
2017-06-02 23:34:11 +0100

    added member model.

---
+++ FelicitySecurity.Applications.Config/FelicitySecurity.Applications.Config/Models/MemberModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FelicitySecurity.Applications.Config.Models
+{
+    public class MemberModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PostCode { get; set; }
+        public DateTime DateOfRegistration { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsMember { get; set; }
+        public bool IsStaff { get; set; }
+        public byte[] FacialImages { get; set; }
+    }
+}
