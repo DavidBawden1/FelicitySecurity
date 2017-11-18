ea48069309787ce027c4461b34e6d2fc41c526c1
Dbawd <Dbawd@DESKTOP-T69TDHC>
2017-11-18 16:33:09 +0000

    moved button into form so i can call form. submit.
    using ajax post. need to sort out data parameters. controller is recieving nulls currently.

--- FelicitySecurity.CCTV/Controllers/HomeController.cs
+++ FelicitySecurity.CCTV/Controllers/HomeController.cs
@@ -16,14 +16,12 @@ namespace FelicitySecurity.CCTV.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
