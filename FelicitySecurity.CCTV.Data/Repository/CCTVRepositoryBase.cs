78e5273782dceac708d42d6d17993309d7c1a649
Dbawd <Dbawd@DESKTOP-T69TDHC>
2017-11-22 22:24:00 +0000

    added repository base and interface for administrator repository

--- FelicitySecurity.CCTV.Data/Repository/CCTVRepositoryBase.cs
+++ FelicitySecurity.CCTV.Data/Repository/CCTVRepositoryBase.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace FelicitySecurity.CCTV.Data
 {
     public class CCTVRepositoryBase
     {
-        private string _connectionString;
-
-        public CCTVRepositoryBase(string connectionString)
+        private DbConnection _connectionString;
+        private Configuration configuration;
+        public CCTVRepositoryBase(Configuration configuration, DbConnection connectionString)
         {
             connectionString = _connectionString;
         }
