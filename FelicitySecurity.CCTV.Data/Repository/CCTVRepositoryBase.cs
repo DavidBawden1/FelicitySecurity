6a9e1754b856aefe03febeaae3462300047b746f
Dbawd <Dbawd@DESKTOP-T69TDHC>
2017-11-22 21:55:33 +0000

    added data project for CCTV.

---
+++ FelicitySecurity.CCTV.Data/Repository/CCTVRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FelicitySecurity.CCTV.Data
+{
+    public class CCTVRepositoryBase
+    {
+        private string _connectionString;
+
+        public CCTVRepositoryBase(string connectionString)
+        {
+            connectionString = _connectionString;
+        }
+    }
+}
