b65517eceabaea075a826665f3624e8d4a791bfe
Dbawd <Dbawd@DESKTOP-T69TDHC>
2017-11-23 18:33:06 +0000

    added repository class.

---
+++ FelicitySecurity.CCTV.Data/Repository/CCTVRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FelicitySecurity.CCTV.Data.Repository
+{
+    public class CCTVRepository : CCTVRepositoryBase
+    {
+        public CCTVRepository(Configuration configuration, string connectionString)
+            : base(configuration, connectionString)
+        {
+
+        }
+    }
+}
