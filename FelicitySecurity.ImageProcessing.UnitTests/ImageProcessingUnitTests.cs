74367c4057e054f23a100d559d3ed8b17abc49d4
David Bawden <Dbawd@DESKTOP-T69TDHC>
2018-01-22 19:44:10 +0000

    added test.

---
+++ FelicitySecurity.ImageProcessing.UnitTests/ImageProcessingUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FelicitySecurity.Applications.Config.Resources.ImageProcessing;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FelicitySecurity.ImageProcessing.UnitTests
+{
+    [TestClass]
+    public class ImageProcessingUnitTests
+    {
+        [TestMethod]
+        public void CameraTakesUniqueImages()
+        {
+            ImageConversions imageConversions = new ImageConversions();
+            Applications.Config.ViewModels.MembersViewModel viewModel = new Applications.Config.ViewModels.MembersViewModel();
+            System.Collections.Generic.List<Image<Gray, byte>> testImageList = new System.Collections.Generic.List<Image<Gray, byte>>();
+
+            viewModel.FacialImages = testImageList;
+            imageConversions.AppendEachImageToByteArrayOfImageList(viewModel);
+        }
+    }
+}
---
+++ FelicitySecurity.ImageProcessing.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FelicitySecurity.ImageProcessing.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FelicitySecurity.ImageProcessing.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b7a15eaa-9dc7-47ef-b302-27ddc50d56b9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
