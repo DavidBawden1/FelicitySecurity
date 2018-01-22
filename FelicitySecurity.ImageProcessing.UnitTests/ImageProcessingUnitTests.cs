ec89d122a1fa38f396fa35d2fed6a6d7254e3b3d
David Bawden <Dbawd@DESKTOP-T69TDHC>
2018-01-22 20:01:55 +0000

    added test need to finish.

--- FelicitySecurity.ImageProcessing.UnitTests/ImageProcessingUnitTests.cs
+++ FelicitySecurity.ImageProcessing.UnitTests/ImageProcessingUnitTests.cs
@@ -10,12 +10,16 @@ namespace FelicitySecurity.ImageProcessing.UnitTests
     public class ImageProcessingUnitTests
     {
         [TestMethod]
-        public void CameraTakesUniqueImages()
+        public void ListOfImagesToByteArray()
         {
             ImageConversions imageConversions = new ImageConversions();
             Applications.Config.ViewModels.MembersViewModel viewModel = new Applications.Config.ViewModels.MembersViewModel();
             System.Collections.Generic.List<Image<Gray, byte>> testImageList = new System.Collections.Generic.List<Image<Gray, byte>>();
-
+            for (int i = 0; i < 20; i++)
+            {
+                Image<Gray, byte> imageToSave;
+                testImageList.Add(imageToSave);
+            }
             viewModel.FacialImages = testImageList;
             imageConversions.AppendEachImageToByteArrayOfImageList(viewModel);
         }
